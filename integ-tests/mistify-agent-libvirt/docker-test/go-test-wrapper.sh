686908169bbc04bc8ff6f58bbe535f7808fc8da5
Ferzan Rizvi <rizvfe00@gmail.com>
2015-11-18 15:50:57 -0500

    Renaming scripts, Moving go gets into dockerfile

--- integ-tests/mistify-agent-libvirt/docker-test/go-test-wrapper.sh
+++ integ-tests/mistify-agent-libvirt/docker-test/go-test-wrapper.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 repo="mistify-agent-libvirt"
-ref=${1-master}
-echo "Using branch $ref"
 
 die() { echo "$@" 1>&2 ; exit 1; }
 
 echo "Add mistify bridge"
-ovs-vsctl --may-exist add-br mistify0
+/add-mistify-bridge.sh
 ovs-vsctl show
 
 checkout_dir="$GOPATH/src/github.com/mistifyio/$repo"
@@ -15,33 +13,7 @@ checkout_dir="$GOPATH/src/github.com/mistifyio/$repo"
 cd $GOPATH
 
 echo "Go get dependencies"
-go get github.com/kisielk/errcheck
-go get github.com/jstemmer/go-junit-report
-go get golang.org/x/tools/cmd/goimports
-go get github.com/golang/lint/golint
 go get -t ./src/github.com/mistifyio/$repo/...
 
 echo "Execute tests"
 go test -v -timeout 30s -p 1 ./src/github.com/mistifyio/$repo/...
-
-clone_repo(){
-    rm -rf $checkout_dir
-
-    git clone https://github.com/mistifyio/mistify-agent-libvirt $checkout_dir
-    if [ $? -gt 0 ]; then
-        die "Cloning repo encountered an error."
-    fi
-
-    cd $checkout_dir
-
-    echo "Checkout branch"
-    git checkout $ref
-    if [ $? -ne 0 ]; then
-        die "Died trying to checkout the repo on: $ref"
-    fi
-
-    git pull
-    if [ $? -ne 0 ]; then
-        die "Died trying to checkout the repo on: $ref"
-    fi
-}
