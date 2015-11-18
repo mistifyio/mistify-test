4838da443b3f2b1ad8eb90cedea458123a07b5e6
Ferzan Rizvi <rizvfe00@gmail.com>
2015-11-18 15:02:11 -0500

    Use jenkins checkout instead of cloning, add die method

--- integ-tests/mistify-agent-libvirt/docker-test/go-test-wrapper.sh
+++ integ-tests/mistify-agent-libvirt/docker-test/go-test-wrapper.sh
@@ -4,29 +4,13 @@ repo="mistify-agent-libvirt"
 ref=${1-master}
 echo "Using branch $ref"
 
+die() { echo "$@" 1>&2 ; exit 1; }
+
 echo "Add mistify bridge"
 ovs-vsctl --may-exist add-br mistify0
+ovs-vsctl show
 
 checkout_dir="$GOPATH/src/github.com/mistifyio/$repo"
-rm -rf $checkout_dir
-
-git clone https://github.com/mistifyio/mistify-agent-libvirt $checkout_dir
-if [ $? -gt 0 ]; then
-    die "Cloning repo encountered an error."
-fi
-
-cd $checkout_dir
-
-echo "Checkout branch"
-git checkout $ref
-if [ $? -ne 0 ]; then
-	die "Died trying to checkout the repo on: $ref"
-fi
-
-git pull
-if [ $? -ne 0 ]; then
-	die "Died trying to checkout the repo on: $ref"
-fi
 
 cd $GOPATH
 
@@ -39,3 +23,25 @@ go get -t ./src/github.com/mistifyio/$repo/...
 
 echo "Execute tests"
 go test -v -timeout 30s -p 1 ./src/github.com/mistifyio/$repo/...
+
+clone_repo(){
+    rm -rf $checkout_dir
+
+    git clone https://github.com/mistifyio/mistify-agent-libvirt $checkout_dir
+    if [ $? -gt 0 ]; then
+        die "Cloning repo encountered an error."
+    fi
+
+    cd $checkout_dir
+
+    echo "Checkout branch"
+    git checkout $ref
+    if [ $? -ne 0 ]; then
+        die "Died trying to checkout the repo on: $ref"
+    fi
+
+    git pull
+    if [ $? -ne 0 ]; then
+        die "Died trying to checkout the repo on: $ref"
+    fi
+}
